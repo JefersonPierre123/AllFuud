f430a3adda2f4654cbdaafec581fbf5d8f589e20
JefersonPierre123 <jefersondasilva@ienh.com.br>
2025-05-26 21:37:17 -0300

    Atualizando componentes registration

--- resources/views/components/establishment-registration-form.blade.php
+++ resources/views/components/establishment-registration-form.blade.php
@@ -1,8 +1,9 @@
-<div class="card">
+<div class="card card-custom">
     <form action="#" class="container mt-4">
         <h2 class="mb-4">Cadastro de Estabelecimentos</h2>
     
         {{-- Grupo: Dados do Estabelecimento --}}
+        <div class="form-section-title"><i class="bi bi-card-list me-2"></i>Dados do Estabelecimento</div>
         <div class="row g-3">
             <div class="col-md-6">
                 <label for="cnpj" class="form-label">CNPJ</label>
@@ -29,6 +30,7 @@
         <hr class="my-4">
     
         {{-- Grupo: Login --}}
+        <div class="form-section-title"><i class="bi bi-lock-fill me-2"></i>Login</div>
         <div class="row g-3">
             <div class="col-md-6">
                 <label for="email" class="form-label">Email</label>
@@ -47,10 +49,13 @@
         <hr class="my-4">
     
         {{-- Grupo: Endereço --}}
+        <div class="form-section-title"><i class="bi bi-geo-alt-fill me-2"></i>Endereço</div>
         <x-address-registration-form />
     
         <div class="mt-4">
-            <x-button variant="primary" type="submit" size="sm">Cadastrar</x-button>
+            <x-button variant="primary" type="submit" size="lg">
+                <i class="bi bi-send-fill me-1"></i> Cadastrar
+            </x-button>
         </div>
 
     </form>
