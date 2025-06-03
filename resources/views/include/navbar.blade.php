06c8f16ced27f6ff44b83e28a215d7994dfcacaa
Vinicius Plack Wecker <viniciuswecker@ienh.com.br>
2025-06-03 16:40:22 -0300

    Atribuição de rota para o link Início e para o simbolo do site. Ambos para a rota index.

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">🍔 AllFuud</a>
+        <a class="navbar-brand" href="{{ route('index') }}">🍔 AllFuud</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                     <a class="nav-link" href="#"><i class="bi bi-house-door-fill me-1"></i>Início</a>
+                     <a class="nav-link" href="{{ route('index') }}"><i class="bi bi-house-door-fill me-1"></i>Início</a>
                 </li>
                 {{-- <li class="nav-item">
                     <a class="nav-link" href="#"><i class="bi bi-journal-text me-1"></i>Cardápio</a>                
